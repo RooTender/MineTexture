13c76334593ecbaaab0865247418b37c03626d05
RooTender <therootender@gmail.com>
2025-09-22 15:26:06 +0200

    best model

--- src/model.py
+++ src/model.py
@@ -2,25 +2,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def conv3(in_c, out_c, stride=1, dilation=1):
-    pad = dilation
-    return nn.Conv2d(in_c, out_c, kernel_size=3, stride=stride, padding=pad, dilation=dilation)
+def conv3(in_c, out_c, stride=1):
+    return nn.Conv2d(in_c, out_c, kernel_size=3, stride=stride, padding=1)
 
 def conv1(in_c, out_c):
     return nn.Conv2d(in_c, out_c, kernel_size=1)
 
 class ResBlock(nn.Module):
-    def __init__(self, ch, dilation=1):
+    def __init__(self, ch):
         super().__init__()
         self.norm1 = nn.InstanceNorm2d(ch, affine=True)
-        self.c1 = conv3(ch, ch, stride=1, dilation=dilation)
+        self.c1 = conv3(ch, ch, stride=1)
+        self.norm2 = nn.InstanceNorm2d(ch, affine=True)
+        self.c2 = conv3(ch, ch, stride=1)
         self.act = nn.SiLU(inplace=True)
 
+        # skalowanie residualu
         self.alpha = nn.Parameter(torch.ones(ch))
+
+        # zero-init drugiej konw
+        nn.init.zeros_(self.c2.weight)
+        if self.c2.bias is not None:
+            nn.init.zeros_(self.c2.bias)
 
     def forward(self, x):
         y = self.c1(self.act(self.norm1(x)))
-        return self.act(x + self.alpha.view(1, -1, 1, 1) * y)
+        y = self.c2(self.act(self.norm2(y)))
+        return self.act(x + self.alpha.view(1,-1,1,1) * y)
     
 class SelfAttention2d(nn.Module):
     def __init__(self, in_ch):
@@ -45,7 +53,6 @@ class SelfAttention2d(nn.Module):
 
         return x + self.gamma * out
 
-
 class TinyUNet(nn.Module):
     """
     64 -> 32 -> 16 -> 8 -> 16 -> 32 -> 64
@@ -61,19 +68,24 @@ class TinyUNet(nn.Module):
 
         self.e2 = conv3(base, base*2, stride=2)     # 16x16
         self.rb2 = ResBlock(base * 2)
-        # self.attn32 = SelfAttention2d(base*2)
+        self.attn32 = SelfAttention2d(base*2)
 
         self.e3 = conv3(base*2, base*4, stride=2)   # 8x8
         self.rb3 = ResBlock(base*4)
 
+        self.e4 = conv3(base*4, base*4, stride=2)   # 4x4 (nie poszerzamy kanałów)
+        self.rb4 = ResBlock(base*4)
 
         # --- BOTTLENECK 4x4 ---
         self.b = nn.Sequential(
-            ResBlock(base*4, dilation=1),
-            SelfAttention2d(base*4),
+            conv3(base*4, base*4, stride=1),
+            ResBlock(base*4),
         )
 
         # --- DECODER ---
+        self.u0 = nn.ConvTranspose2d(base*4, base*4, kernel_size=4, stride=2, padding=1)  # 4->8
+        self.rb_u0 = ResBlock(base*4)
+
         self.u1 = nn.ConvTranspose2d(base*4, base*2, kernel_size=4, stride=2, padding=1)  # 8->16
         self.rb_u1 = ResBlock(base * 2)
 
@@ -115,16 +127,27 @@ class TinyUNet(nn.Module):
 
         y  = self.act(self.e2(y_32))  # 16x16, 2*base
         y  = self.rb2(y)
-        # y  = self.attn32(y)
+        y  = self.attn32(y)
         y_16 = y
 
         y  = self.act(self.e3(y))   # 8x8, 4*base
         y  = self.rb3(y)
+        y_8 = y
+
+        y  = self.act(self.e4(y))   # 4x4, 4*base
+        y  = self.rb4(y)
 
         # Bottleneck
         y  = self.act(self.b(y))    # 4x4
 
         # Decoder
+        # 8x8
+        y = self.act(self.u0(y))
+        y = self.rb_u0(y)
+        skip = self.act(self.skip8_norm(y_8))
+        skip = self.skip8_proj(skip)
+        y = y + self.skip8_alpha * skip
+
         # 16x16
         y = self.act(self.u1(y))
         y = self.rb_u1(y)
